782d3a9516b62d8649008b340e8ec68c59d27855
Scarlett Baez <s.baez1010@gmail.com>
2020-05-31 00:14:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,6 +4,20 @@ function shout('hello') {
 function whisper('HELLO') {
 return string.toLowerCase()
 }
-function logShout('hello') {
-console.log.toUpperCase()
+function logShout('HELLO') {
+console.log ('HELLO')
+}
+function logWhisper('hello') {
+console.log ('hello')
+}
+function sayHiToGrandma('I love you, Grandma.') {
+if (.toLowerCase) {
+  return ("I can't hear you!")
+}
+}
+if (.toUppercase) {
+return ('YES INDEED!')
+}
+if ('I love you, Grandma.') {
+return('I love you too.')
 }
